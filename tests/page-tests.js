389d0f10bcc498d50936505c81407565e110cc6c
nnacin <natali@thebyte9.com>
2020-03-23 21:05:12 +0100

    fix typo, new line, TODO

--- tests/page-tests.js
+++ tests/page-tests.js
@@ -1,5 +1,3 @@
-// TODO check the name, price, correct items for each user type
-
 "use strict";
 
 let PageCatalog = require("../src/page/page-catalog");
@@ -16,7 +14,7 @@ assert.deepEqual(productPrices, [400, 140, 120], "Unexpected result for Child us
 console.log("Child assertion succeed!");
 
 ({ type, discount, limit, products, productPrices } = checkUser("Boy"));
-assert(type === "Boy", "Unexpected result for Boy suer");
+assert(type === "Boy", "Unexpected result for Boy user type");
 assert(discount === 50, "Unexpected result for Boy user discount");
 assert(limit === 300, "Unexpected result for Boy user limit");
 assert.deepEqual(products, ["Laptop", "Tablet", "Móvil"], "Unexpected result for Boy user products");
@@ -24,7 +22,7 @@ assert.deepEqual(productPrices, [1000, 300, 220], "Unexpected result for Boy use
 console.log("Boy assertion succeed!");
 
 ({ type, discount, limit, products, productPrices } = checkUser("Girl"));
-assert(type === "Girl", "Unexpected result for Girl user");
+assert(type === "Girl", "Unexpected result for Girl user type");
 assert(discount === 30, "Unexpected result for Girl user discount");
 assert(limit === 200, "Unexpected result for Girl user limit");
 assert.deepEqual(products, ["Laptop", "Tablet", "Móvil"], "Unexpected result for Girl user products");
@@ -37,7 +35,6 @@ function checkUser(userType) {
     const catalog = new PageCatalog(user);
     const products = catalog.getProducts();
 
-    
     const userProducts = [];
     const userProductPrices = [];
     let product;
